50552ca39fe68ac5496d40feccc4f5e18ce5b10e
leow1534 <146866792+leow1534@users.noreply.github.com>
2023-10-03 12:46:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ownerName = "Leo"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let userName = "leow1534"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
